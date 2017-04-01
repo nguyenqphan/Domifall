72fd93e87c9d5a89ed59dce46ca8aef80c3fba31
Nguyen Phan <nqphan212@gmail.com>
2017-04-01 00:46:59 -0400

    Write a DuplicationCheck script to find the duplicated domino error when placing them

---
+++ Assets/_Scripts/DuplicationCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicationCheck : MonoBehaviour {
+
+	private Transform[] dominoArray;
+
+
+	void Awake()
+	{
+
+		dominoArray = GetComponentsInChildren<Transform>();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 1; i < dominoArray.Length - 1; i++)
+		{
+			if(dominoArray[i].position == dominoArray[i + 1].position)
+			{
+				Debug.Log(dominoArray[i].name);
+			}
+		}
+
+		Debug.Log("The length of dominoes " + dominoArray.Length );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
